0fab9dd992248eede46de7118d905a11196e4a8a
Musquan <musquanmomin@gmail.com>
2025-06-16 10:27:55 -0500

    Modify src/main/java/com/marketplace/onlinemarketplace/service/BidService.java for SCRUM-6

--- src/main/java/com/marketplace/onlinemarketplace/service/BidService.java
+++ src/main/java/com/marketplace/onlinemarketplace/service/BidService.java
@@ -98,5 +98,13 @@ public class BidService {
         }
         return bid;
     }
-}
 
+    public List<Bid> getAllBidsById(Long id) {
+        List<Bid> bids = bidRepo.findById(id);
+        if (bids.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return bids;
+    }
+
+}
