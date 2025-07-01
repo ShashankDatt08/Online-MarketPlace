ebd5c0c6d2c915a7a6d7f6d8cb5432402b44d565
Musquan Karovalia <71215436+Musquan@users.noreply.github.com>
2025-07-01 11:16:10 -0500

    Add method to get all bids by ID in BidService

---
+++ src/services/BidService.java
@@ -0,0 +1,16 @@
+public class BidService {
+    // Existing methods...
+
+    /**
+     * Retrieves all bids associated with a specific ID.
+     *
+     * @param id The ID to retrieve bids for.
+     * @return A list of bids associated with the given ID.
+     */
+    public List<Bid> getAllBidsById(String id) {
+        // Implementation to retrieve bids by ID
+        List<Bid> bids = new ArrayList<>();
+        // Logic to populate bids list
+        return bids;
+    }
+}
